9063cc677ef2e21d0fd3d6145c2f1767d0fa6f5d
chiyi4488 <chiyi4869@gmail.com>
2018-11-06 17:39:55 +0800

    Update Go_Top.js

--- website/Go_Top.js
+++ website/Go_Top.js
@@ -1,5 +1,6 @@
 <!-- Go Top 按鈕+動畫 start -->
 <img id="goTop" src="https://raw.githubusercontent.com/chiyi4488/Yi-s-Blog/master/back-to-top(big).png" title="Back to Top"/>
+<script>
 $.extend($.easing, {
 easeOutExpo: function (x, t, b, c, d) {
 return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
@@ -8,5 +9,5 @@ return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
 $("#goTop").click(function(){
 $("html, body").animate({scrollTop: 0}, 1000, "easeOutExpo");
 });
-
-<!-- Go Top 按鈕+動畫 end, Designed by WFU BLOG <script></script>-->
+</script>
+<!-- Go Top 按鈕+動畫 end, Designed by WFU BLOG -->
