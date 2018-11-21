d33cc5e0964da86853cb076e79f34f2cf15e2a4e
chiyi4488 <chiyi4869@gmail.com>
2018-11-21 17:44:59 +0800

    Create test2.js

---
+++ website/test2.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+
+  <body>
+    <div class="container">
+      <h2>Music Glyph</h2>
+      <p>Music icon: <span class="glyphicon glyphicon-music"></span></p>    
+      <p>Music icon as a link:
+        <a href="#">
+          <span class="glyphicon glyphicon-music"></span>
+        </a>
+      </p>
+      <p>Music icon on a button:
+        <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon-music"></span> Music
+        </button>
+      </p>
+      <p>Music icon on a styled link button:
+        <a href="#" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-music"></span> Music
+        </a>
+      </p> 
+    </div>
+
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+
+</html>
