b5829fda0c7e6fe2a95721cfeba62409a9bd6046
chiyi4488 <chiyi4869@gmail.com>
2018-11-21 17:42:12 +0800

    Update Go_Top.js

--- website/Go_Top.js
+++ website/Go_Top.js
@@ -6,21 +6,21 @@
     /* Go Top 按鈕 */
     #goTop {
     position: fixed;
-    bottom: 5px; /* 與下方的距離, 也可改為百分比, 即為距離螢幕下方的百分比 */
-    right: 5px; /* 與右方的距離 */
-    width: 40px; /* 按鈕原始寬度 */
-    height: 40px; /* 按鈕原始高度 */
-    opacity: 0.4; /* 按鈕原始透明度 */
-    z-index: 10;
+    bottom: 5px ;
+    right: 5px ; 
+    width: 40px ; 
+    height: 40px ; 
+    opacity: 0.4 ; 
+    z-index: 10 ;
     cursor: pointer;
-    transition: all .5s; /* 動畫效果 持續期間 */
+    transition: all .5s; 
     -webkit-transition: all .5s;
     -moz-transition: all .5s;
     }
-    #goTop:hover { /* 滑鼠經過按鈕時 */
-    opacity: 1; /* 透明度 */
-    width: 80px; /* 按鈕寬度 */
-    height: 80px; /* 按鈕高度 */
+    #goTop:hover { 
+    opacity: 1; 
+    width: 80px; 
+    height: 80px; 
     }
     </style>
   </head>
